acb8ae3d03384f26877206244c6f93450c0ca792
Veenith31 <147136881+Veenith31@users.noreply.github.com>
2025-08-29 14:10:17 +0530

    PdfPage.js

--- summarizer-frontend/src/pages/PdfPage.js
+++ summarizer-frontend/src/pages/PdfPage.js
@@ -1,20 +1,4 @@
-/*import React from 'react';
-import Sidebar from '../components/Sidebar';
-import PdfSummarizationPage from './PdfSummarization';
 
-const PdfPage = () => {
-  return (
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <Sidebar />
-      <div style={{ flex: 1, padding: '20px', overflowY: 'auto' }}>
-        <PdfSummarizationPage />
-      </div>
-    </div>
-  );
-};
-
-export default PdfPage;
-*/
 import React from 'react';
 import LayoutWithSidebar from '../components/LayoutWithSidebar';
 import PdfSummarizationPage from './PdfSummarization';
