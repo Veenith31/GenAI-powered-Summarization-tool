0bb1cd1fb47c06e9c9a2bda225a471c51556effa
Veenith31 <147136881+Veenith31@users.noreply.github.com>
2025-10-08 19:06:43 +0530

    Update OcrController.java

--- src/main/java/com/summarizer/project/controller/OcrController.java
+++ src/main/java/com/summarizer/project/controller/OcrController.java
@@ -1,31 +1,3 @@
-//package com.summarizer.project.controller;
-//
-//import com.summarizer.project.model.Summary;
-//import com.summarizer.project.service.OcrService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//@RestController
-//@RequestMapping("/api/ocr")
-//public class OcrController {
-//
-//    @Autowired
-//    private OcrService ocrService;
-//
-//    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    public ResponseEntity<Summary> uploadImageAndSummarize(
-//            @RequestParam("file") MultipartFile file,
-//            @RequestParam("userId") String userId) {
-//
-//        Summary summary = ocrService.processImage(file, userId);
-//        return ResponseEntity.ok(summary);
-//    }
-//}
-
-
 package com.summarizer.project.controller;
 
 import com.summarizer.project.service.OcrService;
