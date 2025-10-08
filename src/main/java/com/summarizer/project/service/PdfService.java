b431d9eb06b871cbe4e319cbb6e52efda370e67e
Veenith31 <147136881+Veenith31@users.noreply.github.com>
2025-10-08 19:10:03 +0530

    Update PdfService.java

--- src/main/java/com/summarizer/project/service/PdfService.java
+++ src/main/java/com/summarizer/project/service/PdfService.java
@@ -1,25 +1,3 @@
-/*package com.summarizer.project.service;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-@Service
-public class PdfService {
-
-    public String extractTextFromPdf(InputStream inputStream) throws IOException {
-        PDDocument document = PDDocument.load(inputStream);
-        PDFTextStripper pdfStripper = new PDFTextStripper();
-        String text = pdfStripper.getText(document);
-        document.close();
-        return text;
-    }
-}
-*/
-
 package com.summarizer.project.service;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
